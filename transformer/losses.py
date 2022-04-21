filter_pn plus bugfix in log_d calculation

transformer/losses.py
@@ -62,13 +62,13 @@ def log_prp_loss(logprobs, mask_nonzero, ispositive):
             
     if ispositive:
         log_n = LogOneMinusSumExp(logprobs, mask_nonzero)
-        log_d = tf.reduce_sum(logprobs, axis=-1) / k
+        log_d = tf.reduce_sum(mask_nonzero * logprobs, axis=-1) / k
         loss = log_n - log_d
         # loss *= 1.0 - sumprob
     else:
         log_n = LogSumExp(logprobs, -1, mask_nonzero)
         logprobs2 = tf.maximum(logEPS, logprobs)
-        log_d = tf.reduce_sum(logprobs2, axis=-1, keepdims=True) / k
+        log_d = tf.reduce_sum(mask_nonzero * logprobs2, axis=-1, keepdims=True) / k
         loss = log_d + log_n
         # loss *= sumprob
 
@@ -135,9 +135,6 @@ def loss_function(real, pred, ispositive, loss_type):
         
 
 def loss_function_joint(real_pos, real_neg, pred_pos, pred_neg):
-    print(real_pos)
-    print(real_neg)
-    xxx
     pos_logprobs, pos_mask = get_sequence_logprobs(real_pos, pred_pos)
     neg_logprobs, neg_mask = get_sequence_logprobs(real_neg, pred_neg)
 

transformer/train.py
@@ -6,12 +6,24 @@
 # gpu = gpus[np.random.randint(0, gpu_count)]
 # os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 
+import tensorflow as tf
+print("GPU available: ", tf.config.list_physical_devices('GPU'))
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+  # Invalid device or cannot modify virtual devices once initialized.
+  pass
+
+import logging
+logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
 
 import time
 import sys
-import transformer
-import tensorflow as tf
 import argparse
+
+import transformer
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--datadir', type=str, required=True)
 parser.add_argument('--epochs', type=int, default=100)
@@ -32,7 +44,8 @@ parser.add_argument('--loss_type', type=str, default="lprp") #"nll", "prp", "lpr
 parser.add_argument('--split', type=str, default="0.7,0.15,0.15")
 parser.add_argument('--outdir', type=str)
 parser.add_argument('--monitor_probs', type=int, default=0)
-parser.add_argument('--seed', type=int, default=1000)
+parser.add_argument('--filter_pn', type=int, default=0)
+parser.add_argument('--seed', type=int, default=100)
 
 
 args = parser.parse_args()
@@ -48,6 +61,7 @@ BEAMSIZE=args.beamsize
 LOSS_TYPE=args.loss_type
 SPLIT=[float(x) for x in args.split.split(",")]
 MONITOR_PROBS = args.monitor_probs == 1
+FILTER_PN = args.filter_pn == 1
 OUTDIR = args.outdir
 
 # tokenizer parameters
@@ -180,6 +194,7 @@ else:
 transformer.train(EPOCHS, my_transformer, optimizer, pos_batches, neg_batches, NEG_WEIGHT, LOSS_TYPE,
                   outdir=OUTDIR,
                   monitor_probs=MONITOR_PROBS,
+                  filter_pn=FILTER_PN,
                   ckpt_manager=ckpt_manager)
 
 my_transformer.summary()

transformer/transformer.py
@@ -10,7 +10,6 @@ LENGTH_PENALTY=0.5
 SUPPORT_SIZE=50
 
 import collections
-import logging
 import os
 import pathlib
 import re
@@ -27,14 +26,6 @@ from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 
 from monitor import MonitorProbs
 
-print("GPU available: ", tf.config.list_physical_devices('GPU'))
-physical_devices = tf.config.list_physical_devices('GPU')
-try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-except:
-  # Invalid device or cannot modify virtual devices once initialized.
-  pass
-logging.getLogger('tensorflow').setLevel(logging.ERROR)  # suppress warnings
 
 
 ##########################################
@@ -509,6 +500,19 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
     return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
+def filter_posneg(real_pos, real_neg):
+    # real_pos, real_neg: (support * bs * seq_len)
+
+    pos = tf.expand_dims(real_pos, axis=1)
+    neg = tf.expand_dims( real_neg, axis=0)
+    match = tf.equal(pos, neg)
+    match = tf.math.reduce_all(match, axis=3, keepdims=True)
+    match_pos = tf.math.reduce_any(match, axis=1, keepdims=False)
+    mask = tf.cast(tf.math.logical_not(match_pos), dtype=real_pos.dtype)
+    real_pos *= mask
+    return real_pos, real_neg
+
+
 
 
 # The @tf.function trace-compiles train_step into a TF graph for faster
@@ -531,6 +535,7 @@ train_step_signature_noneg = [
 
 def train(epochs, transformer, optimizer, pos_batches, neg_batches, neg_weight, loss_type,
           monitor_probs=False,
+          filter_pn=False,
           outdir=None,
           ckpt_manager=None):
 
@@ -552,7 +557,11 @@ def train(epochs, transformer, optimizer, pos_batches, neg_batches, neg_weight, 
         with tf.GradientTape() as tape:            
             pos_predictions = tf.map_fn(get_prediction, pos_tar, fn_output_signature=tf.TensorSpec(shape=[None, None, None], dtype=tf.float32), parallel_iterations=10)
             neg_predictions = tf.map_fn(get_prediction, neg_tar, fn_output_signature=tf.TensorSpec(shape=[None, None, None], dtype=tf.float32), parallel_iterations=10)
+            
 
+            if filter_pn:
+                pos_tar_real, neg_tar_real = filter_posneg(pos_tar_real, neg_tar_real)
+            
             if loss_type == "joint_lprp":
                 loss, pos_probs, neg_probs = loss_function_joint(pos_tar_real, neg_tar_real, pos_predictions, neg_predictions)
                 pos_loss = loss
@@ -572,7 +581,7 @@ def train(epochs, transformer, optimizer, pos_batches, neg_batches, neg_weight, 
         train_neg_loss(neg_loss)
         train_neg_probs(neg_probs)
 
-        if monitor_probs:
+        if monitor_probs and (loss_type != "joint_lprp"):
             monitor.update(inp, pos_tar_real, pos_sequence_probs)
         
         return pos_predictions